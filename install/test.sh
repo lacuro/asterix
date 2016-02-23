a2a5b425b964534bea8ecf691c0a383ce48d2def
Damir Salantić <damir.salantic@gmail.com>
2016-02-23 17:51:53 +0000

    Tests updated

--- install/test.sh
+++ install/test.sh
@@ -15,13 +15,13 @@ test () {
 	fi
 }
 
-test "Test filter" "$ASTERIX -d config/asterix.ini -L sample_data/filter.txt" "test_output/filter.txt"
+test "Test filter" "$ASTERIX -d config/asterix.ini -L test_output/filter.txt" "test_output/filter.txt"
 test "Test json CAT_062_065" "$ASTERIX -d config/asterix.ini -P -j -f sample_data/cat_062_065.pcap" "test_output/cat_062_065_json.txt"
-test "Test json CAT_001_002" "$ASTERIX -d config/asterix.ini -R -j -f sample_data/cat_001_002.pcap -LF sample_data/filter.txt" "test_output/cat_001_002_json.txt"
-test "Test jsonh CAT_001_002" "$ASTERIX -d config/asterix.ini -R -jh -f sample_data/cat_001_002.pcap -LF sample_data/filter.txt" "test_output/cat_001_002_jsonh.txt"
-test "Test xml CAT_001_002" "$ASTERIX -d config/asterix.ini -R -x -f sample_data/cat_001_002.pcap -LF sample_data/filter.txt" "test_output/cat_001_002.xml"
-test "Test txt CAT_001_002" "$ASTERIX -d config/asterix.ini -R -f sample_data/cat_001_002.pcap -LF sample_data/filter.txt" "test_output/cat_001_002.txt"
-test "Test line CAT_001_002" "$ASTERIX -d config/asterix.ini -R -l -f sample_data/cat_001_002.pcap -LF sample_data/filter.txt" "test_output/cat_001_002_line.txt"
+test "Test json CAT_001_002" "$ASTERIX -d config/asterix.ini -R -j -f sample_data/cat_001_002.pcap -LF test_output/filter.txt" "test_output/cat_001_002_json.txt"
+test "Test jsonh CAT_001_002" "$ASTERIX -d config/asterix.ini -R -jh -f sample_data/cat_001_002.pcap -LF test_output/filter.txt" "test_output/cat_001_002_jsonh.txt"
+test "Test xml CAT_001_002" "$ASTERIX -d config/asterix.ini -R -x -f sample_data/cat_001_002.pcap -LF test_output/filter.txt" "test_output/cat_001_002.xml"
+test "Test txt CAT_001_002" "$ASTERIX -d config/asterix.ini -R -f sample_data/cat_001_002.pcap -LF test_output/filter.txt" "test_output/cat_001_002.txt"
+test "Test line CAT_001_002" "$ASTERIX -d config/asterix.ini -R -l -f sample_data/cat_001_002.pcap -LF test_output/filter.txt" "test_output/cat_001_002_line.txt"
 
 if [ "$failedtests" == "0" ]; then
   echo "All tests OK"
